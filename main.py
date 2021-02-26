b775114da6b0e6af5750cbf13cd162911441cedf
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-02-26 15:15:57 -0300

    final

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ tim = t.Turtle()
 screen = t.Screen()
 tim.pensize(15)
 tim.speed("fastest")
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-           "SeaGreen"]
+t.colormode(255)
+
+def random_color():
+    r = (random.randint(0, 255))
+    g = (random.randint(0, 255))
+    b = (random.randint(0, 255))
+    random_color(r, g, b)
+    return random_color
 
 
 def walk():
     var = [0, 90, 180, 270]
     tim.forward(20)
     tim.setheading(random.choice(var))
-    tim.color(random.choice(colours))
+    tim.color(random_color())
 
 
 for i in range(200):
